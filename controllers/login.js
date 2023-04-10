873984a2ecaa5b750606f15114a6b92d48104dbe
serhatprogramming <serhatprogramming@gmail.com>
2023-04-10 12:48:11 -0400

    id is added to the login response

--- controllers/login.js
+++ controllers/login.js
@@ -26,7 +26,7 @@ loginRouter.post("/", async (request, response, next) => {
 
     response
       .status(200)
-      .send({ token, username: user.username, name: user.name });
+      .send({ token, username: user.username, name: user.name, id: user.id });
   } catch (error) {
     next(error);
   }
